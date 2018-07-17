b998c8e90c14595d25f2833a525d992df1c41c1b
Nicholas Kroft <stangs56@gmail.com>
2018-07-16 23:33:14 -0400

    added python test file

---
+++ test.py
@@ -0,0 +1,25 @@
+from multiprocessing import Process
+
+import ShiftRegister
+import CRCGenerator
+import Encoder4b5b
+import Decoder4b5b
+
+def test(inst):
+    inst.simulate()
+
+def main():
+    tests = [ShiftRegister.ShiftRegister(),
+        # CRCGenerator.CRCGenerator(),
+        Encoder4b5b.Encoder4b5b(),
+        Decoder4b5b.Decoder4b5b()]
+
+    # Processes need to be created as myhdl only
+    # allows 1 simulation per python instance
+    for cur in tests:
+        p = Process(target=test, args=(cur,))
+        p.start()
+        p.join()
+
+if __name__ == '__main__':
+    main()
