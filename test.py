e901076a5ad8fbb6e6bda4640db68e59d9671de5
Nicholas Kroft <stangs56@gmail.com>
2018-07-19 14:44:15 -0400

    Added CRCGenerator to test file

--- test.py
+++ test.py
@@ -1,18 +1,21 @@
-from multiprocessing import Process
+from multiprocessing import Process, JoinableQueue
 
 import ShiftRegister
 import CRCGenerator
 import Encoder4b5b
 import Decoder4b5b
+import sys
 
 def test(inst):
     inst.simulate()
 
 def main():
     tests = [ShiftRegister.ShiftRegister(),
-        # CRCGenerator.CRCGenerator(),
+        CRCGenerator.CRCGenerator(),
         Encoder4b5b.Encoder4b5b(),
         Decoder4b5b.Decoder4b5b()]
+
+    hadFailure = False
 
     # Processes need to be created as myhdl only
     # allows 1 simulation per python instance
@@ -20,6 +23,10 @@ def main():
         p = Process(target=test, args=(cur,))
         p.start()
         p.join()
+        if p.exitcode != 0:
+            hadFailure = True
+
+    sys.exit(1 if hadFailure else 0)
 
 if __name__ == '__main__':
     main()
