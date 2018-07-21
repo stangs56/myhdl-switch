09ff1355c22226a327f483fc7427d8f96c48054f
Nicholas Kroft <stangs56@gmail.com>
2018-07-20 23:31:57 -0400

    limited imports for decode and encode 4b5b

--- Decoder4b5b.py
+++ Decoder4b5b.py
@@ -1,6 +1,6 @@
 from myhdl import *
 from HardwareBlock import *
-from Constants import *
+from Constants import decodingTable4b5b
 
 class Decoder4b5b(HardwareBlock):
     def __init__(self, signals = None):
